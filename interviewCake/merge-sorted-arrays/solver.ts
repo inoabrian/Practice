d7ec9feac98e0467b911b68f3104b1ba242ac0c0
inoabrian <brianmust1@gmail.com>
2017-03-01 13:49:10 -0500

    Added two new problems from interview cake

---
+++ interviewCake/merge-sorted-arrays/solver.ts
@@ -0,0 +1,10 @@
+/* 
+    https://www.interviewcake.com/question/python/merge-sorted-arrays
+
+    QUESTION:
+    In order to win the prize for most cookies sold, my friend Alice and I are going to merge our Girl Scout Cookies orders and enter as one unit.
+    Each order is represented by an "order id" (an integer).
+
+    We have our lists of orders sorted numerically already, in lists. Write a function to merge our lists of orders into one sorted list.
+*/
+
