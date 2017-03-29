48430e7f7b612b5797fe13d9fcbb4e604e1af193
Brian Inoa <Brianmust1@gmail.com>
2017-03-29 15:25:25 -0400

    Added documentation for the problem.

---
+++ interviewCake/inflight-entertainment/solver.ts
@@ -0,0 +1,12 @@
+// https://www.interviewcake.com/question/csharp/inflight-entertainment?utm_source=weekly_email&utm_campaign=weekly_email&utm_medium=email
+
+/*
+  Write a function that takes an integer flightLength (in minutes) and an array of integers movieLengths (in minutes) and returns a boolean indicating whether there are two numbers in movieLengths whose sum equals flightLength.
+
+  When building your function:
+
+  Assume your users will watch exactly two movies
+  Don't make your users watch the same movie twice
+  Optimize for runtime over memory
+  
+*/
